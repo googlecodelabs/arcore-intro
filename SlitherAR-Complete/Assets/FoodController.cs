9a8d69cf6a2a0a21062502534975cb30ef4e4aad
Clayton Wilkinson <wilkinsonclay@google.com>
2017-09-27 12:22:22 -0700

    Formatting FoodController.cs
    Change-Id: Id5e4da78db3e406f41ba8ccd3d25ced63bce1868

--- SlitherAR-Complete/Assets/FoodController.cs
+++ SlitherAR-Complete/Assets/FoodController.cs
@@ -3,66 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using GoogleARCore;
 
-public class FoodController : MonoBehaviour {
-  private TrackedPlane trackedPlane;
-  private GameObject foodInstance;
-  private float foodAge;
-  private readonly float maxAge = 10f;
-  public GameObject[] foodModels;
+public class FoodController : MonoBehaviour
+{
+	private TrackedPlane trackedPlane;
+	private GameObject foodInstance;
+	private float foodAge;
+	private readonly float maxAge = 10f;
+	public GameObject[] foodModels;
 
 
-  public void SetSelectedPlane(TrackedPlane selectedPlane)
-  {
-    trackedPlane = selectedPlane;
-  }
+	public void SetSelectedPlane (TrackedPlane selectedPlane)
+	{
+		trackedPlane = selectedPlane;
+	}
 
-    // Use this for initialization
-    void Start () {
-    }
+	// Use this for initialization
+	void Start ()
+	{
+	}
 
-    // Update is called once per frame
-    void Update () {
-    if (trackedPlane == null) {
-      return;
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		if (trackedPlane == null) {
+			return;
+		}
 
-    if (!trackedPlane.IsValid) {
-      return;
-    }
+		if (!trackedPlane.IsValid) {
+			return;
+		}
 			
-    if (foodInstance == null || foodInstance.activeSelf == false) {
-      SpawnFoodInstance();
-      return;
-    }
+		if (foodInstance == null || foodInstance.activeSelf == false) {
+			SpawnFoodInstance ();
+			return;
+		}
 
-    foodAge += Time.deltaTime;
-    if (foodAge >= maxAge) {
-      DestroyObject(foodInstance);
-      foodInstance = null;
-    }
-  }
+		foodAge += Time.deltaTime;
+		if (foodAge >= maxAge) {
+			DestroyObject (foodInstance);
+			foodInstance = null;
+		}
+	}
 
-  private void SpawnFoodInstance() {
-    GameObject foodItem = foodModels[Random.Range(0,foodModels.Length-1)];
+	private void SpawnFoodInstance ()
+	{
+		GameObject foodItem = foodModels [Random.Range (0, foodModels.Length - 1)];
 
-    // Pick a location.  This is done by selecting a vertex at random and then
-	// a random point between it and the center of the plane.
-		List<Vector3> vertices = new List<Vector3>();
+		// Pick a location.  This is done by selecting a vertex at random and then
+		// a random point between it and the center of the plane.
+		List<Vector3> vertices = new List<Vector3> ();
 		trackedPlane.GetBoundaryPolygon (ref vertices);
 		Vector3 pt = vertices [Random.Range (0, vertices.Count - 1)];
 		float dist = Random.Range (0.05f, 1f);
 		Vector3 position = Vector3.Lerp (pt, trackedPlane.Position, dist);
 
-    Anchor anchor = Session.CreateAnchor(position, Quaternion.identity);
+		Anchor anchor = Session.CreateAnchor (position, Quaternion.identity);
 
-    foodInstance = Instantiate(foodItem,position, Quaternion.identity,anchor.transform);
+		foodInstance = Instantiate (foodItem, position, Quaternion.identity, anchor.transform);
 
-    // Set the tag
-    foodInstance.tag = "food";
+		// Set the tag
+		foodInstance.tag = "food";
 
-     foodInstance.transform.localScale = new Vector3(.01f,.01f,.01f);
-    foodAge = 0;
+		foodInstance.transform.localScale = new Vector3 (.01f, .01f, .01f);
+		foodAge = 0;
 
-    foodInstance.AddComponent<FoodMotion>();
-  }
+		foodInstance.AddComponent<FoodMotion> ();
+	}
 }
