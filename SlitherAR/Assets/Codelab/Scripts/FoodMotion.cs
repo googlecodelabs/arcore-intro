3b8df8414e86f942833b7b8304cb326dfa8ef628
Clayton Wilkinson <wilkinsonclay@google.com>
2017-09-27 12:20:12 -0700

    Adding starter assets.
    Change-Id: I088826f64666b952201269f99e4d3e40a35c3c6e

---
+++ SlitherAR/Assets/Codelab/Scripts/FoodMotion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Food motion - rotates the object around the Y axis.
+/// </summary>
+public class FoodMotion : MonoBehaviour
+{
+  float speed = 30;
+
+  void Update ()
+  {
+    transform.Rotate (Vector3.down, Time.deltaTime * speed * 5);
+  }
+}
